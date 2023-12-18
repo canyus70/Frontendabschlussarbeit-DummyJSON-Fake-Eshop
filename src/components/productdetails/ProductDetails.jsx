3578d59de6713853004ed91f92a04f6f1137d29e
cecilestaller <c.staller96@gmail.com>
2023-12-18 16:28:04 +0100

    detail page und path in app jsx

--- src/components/productdetails/ProductDetails.jsx
+++ src/components/productdetails/ProductDetails.jsx
@@ -1,5 +1,62 @@
+import { useContext } from "react";
+import { Products } from "./../../context/Context";
+import { Link, useParams } from "react-router-dom";
+import Star from './../svg/Star';
+import LeftArrow from "../svg/LeftArrow";
+
+
+
+
 const ProductDetails = () => {
-    return ( <article></article> );
+    const context = useContext(Products);
+
+    const path = useParams();
+    const completePath = path.id;
+
+    const productArr = [...context.products];
+
+    const filteredProductDetail = productArr.filter((singleProductObj) => {
+        return singleProductObj.id.toString() === completePath.toString();
+    })
+
+    const navigateToPreviousPage = () => {
+        
+    }
+
+    return ( 
+        <>
+            {filteredProductDetail.map((product, index) => (
+                <article key={index}>
+                    <div>
+                        <div className="leftArrow_wrap" onClick={navigateToPreviousPage}> <LeftArrow /> </div>
+                        <h2>{product.title}</h2>
+                    </div>
+                    
+                    <div>
+                        <img src={product.thumbnail} alt={product.title} />
+                        <article>
+                            <div>
+                                <h3>{product.title}</h3>
+                                <div className="quantityCounter_wrap">
+                                    <button>-</button>
+                                    <p>1</p>
+                                    <button>+</button>
+                                </div>
+                            </div>
+                            <p> <Star /> {product.rating}</p>
+                            <div>
+                                <p>{product.stock} pieces in stock</p>
+                                <h4>${product.price}</h4>
+                            </div>
+                        </article>
+                    </div>
+                    <h2>Description</h2>
+                    <p>{product.description}</p>
+                    <button>Add to Cart</button>
+                </article>
+            ))}
+        </> 
+    );
 }
  
 export default ProductDetails;
