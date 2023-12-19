1df637d5268eeb52b154f107d7cec308c27f8e44
KarakayaMetin8787 <karakayametin8787@gmail.com>
2023-12-19 12:03:18 +0100

    changed duplicate imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from "react";
-import { useContext, useState } from "react";
 import { Products } from "../context/Context";
 import Search from "../components/search/Search";
 import CategoryBar from "../components/categorybar/CategoryBar";
