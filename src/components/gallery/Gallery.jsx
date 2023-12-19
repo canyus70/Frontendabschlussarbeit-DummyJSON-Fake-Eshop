90563a150cdc89253595638a273b9260550c6761
Samuel Aliyari <samuelaliyari@outlook.de>
2023-12-19 09:28:19 +0100

    Search and Category Bar

---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,22 @@
+import "./Gallery.scss";
+import ProductItem from "../productitem/ProductItem";
+import { useEffect } from "react";
+
+const Gallery = ({ renderProducts, id }) => {
+	useEffect(() => {}, [renderProducts]);
+	return (
+		<section className='gallery'>
+			{renderProducts.map((product) => (
+				<ProductItem
+					key={product.id}
+					title={product.title}
+					price={Number(product.price).toFixed(2)}
+					image={product.thumbnail}
+					rating={product.rating}
+				/>
+			))}
+		</section>
+	);
+};
+
+export default Gallery;
